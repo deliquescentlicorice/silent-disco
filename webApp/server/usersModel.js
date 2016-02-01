6b00ebfcf6d3cdbcc17c9421a322ebbb3e8790a9
Max Kahn <kahn.max@gmail.com>
2016-02-01 10:39:04 -0800

    moved files into models / controllers

--- webApp/server/usersModel.js
+++
@@ -1,8 +0,0 @@
-var MongoClient = require('mongodb');
-var mongoose = require('mongoose');
-
-var userSchema = new mongoose.Schema({
-  name: String
-});
-
-module.exports = mongoose.model('User', userSchema);
