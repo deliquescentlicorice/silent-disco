a7006d8f18e63b5c98f1a6d0bae8f1f95f5d6425
Steven Law <slaw@nuclearanvil.com>
2016-02-01 23:22:39 -0800

    Added callback route for OAuth. - Also, formatting fix.

--- webApp/server/routes.js
+++ webApp/server/routes.js
@@ -2,17 +2,26 @@ var streamsController = require('./streamsController');
 var usersController = require('./usersController');
 var path = require('path');
 var express = require('express');
+var passport = require('passport');
 
-module.exports = function(app, express) {
+module.exports = function(app, express, ensureAuth) {
+  app.post('/api/:stream', streamsController.createStream);
+  app.get('/api/listen/:stream', streamsController.getStream);
+  app.put('/api/listen/:stream', streamsController.upHeart);
+  app.put('/api/broadcast/:stream', streamsController.modifyStreamDetails);
+  app.get('/api/streams', streamsController.getAllStreams);
 
+  app.get('/broadcast', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public', 'index.html'));
+  });
 
-    app.post('/api/:stream', streamsController.createStream);
-    app.get('/api/listen/:stream', streamsController.getStream);
-    app.put('/api/listen/:stream', streamsController.upHeart);
-    app.put('/api/broadcast/:stream', streamsController.modifyStreamDetails);
-    app.get('/api/streams', streamsController.getAllStreams);
+  app.get('/auth/soundcloud', function(req, res) {
+    // on success, closes out the pop-up
+    // res.redirect('/broadcast/setup')
+    res.sendFile(path.join(__dirname + '/../public', 'success.html'));
+  });
 
-    app.get('/broadcast', function(req, res) {
-      res.sendFile(path.join(__dirname + '/../public', 'index.html'));
+  app.get('/user', ensureAuth, function (req, res){
+    res.json(req.user);
   });
 };
