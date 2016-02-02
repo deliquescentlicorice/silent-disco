d2ab13e30b85f34999225a2ffec16e97af37e402
Max Kahn <kahn.max@gmail.com>
2016-02-01 18:50:08 -0800

    changed names of routes for consistency with static files

--- webApp/server/routes.js
+++ webApp/server/routes.js
@@ -5,6 +5,11 @@ var express = require('express');
 var passport = require('passport');
 
 module.exports = function(app, express, ensureAuth) {
+  app.post('/api/:stream', streamsController.createStream);
+  app.get('/api/stream/:stream', streamsController.getStream);
+  app.put('/api/stream/:stream', streamsController.upHeart);
+  app.put('/api/deejay/:stream', streamsController.modifyStreamDetails);
+  app.get('/api/streams', streamsController.getAllStreams);
 
   app.get('/broadcast', function(req, res) {
     res.sendFile(path.join(__dirname + '/../public', 'index.html'));
