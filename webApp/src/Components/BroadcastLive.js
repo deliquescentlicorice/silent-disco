8e310022ffa5a5a6de7ac42cb89985d11e232c0b
Steven Law <slaw@nuclearanvil.com>
2016-02-03 16:40:53 -0800

    Add in broadcasting functionality from test broadcast client.

--- webApp/src/Components/BroadcastLive.js
+++ webApp/src/Components/BroadcastLive.js
@@ -6,11 +6,13 @@ import CardActions from '../../node_modules/material-ui/lib/card/card-actions';
 import CardHeader from '../../node_modules/material-ui/lib/card/card-header';
 import CardMedia from '../../node_modules/material-ui/lib/card/card-media';
 import CardTitle from '../../node_modules/material-ui/lib/card/card-title';
+import DropDownMenu from '../../node_modules/material-ui/lib/DropDownMenu';
 import FlatButton from '../../node_modules/material-ui/lib/flat-button';
 import FloatingActionButton from '../../node_modules/material-ui/lib/floating-action-button';
 import Favorite from '../../node_modules/material-ui/lib/svg-icons/action/favorite';
 import Face from '../../node_modules/material-ui/lib/svg-icons/action/face';
 import TrendingUp from '../../node_modules/material-ui/lib/svg-icons/action/trending-up';
+import MenuItem from '../../node_modules/material-ui/lib/menus/menu-item';
 import Mic from '../../node_modules/material-ui/lib/svg-icons/av/mic';
 import MicOff from '../../node_modules/material-ui/lib/svg-icons/av/mic-off';
 import CardText from '../../node_modules/material-ui/lib/card/card-text';
@@ -27,7 +29,6 @@ import reactMixin from 'react-mixin';
 // COMPONENTS
 import NavBar from './NavBar.js';
 
-
 class BroadcastLive extends React.Component {
   constructor(props) {
     var user = JSON.parse(localStorage.getItem("me"));
@@ -40,19 +41,74 @@ class BroadcastLive extends React.Component {
       description: "Description of the station",
       artist: user.full_name || "anonymous",
       artistAlias: user.username || "QuantumRadio Broadcaster",
-      artistImage: user.avatar_url
+      artistImage: user.avatar_url,
+      selectedSource: null,
+      audioSources: []
     };
   }
 
-  startBroadcast() {
+  gotMediaDevices(devices) {
+    var audioSources = [];
+    console.log('GMD was called');
+    devices.forEach(function(device) {
+      if (device.kind === 'audio') {
+        audioSources.push({
+          id: device.id,
+          label: device.label || 'Input ' + audioSources.length + 1
+        })
+      }
+    });
     this.setState({
-      disabled:true
-    })
+      audioSources: audioSources
+    });
+  }
+
+  componentWillMount() {
+    console.log(this.props.location.state.streamId);
+    // var streamId = 44;
+    this.bc = new Broadcaster(this.props.location.state.streamId, this.gotMediaDevices.bind(this), function(data) {
+        var canvas = document.getElementById("canvas"),
+            width = canvas.width,
+            height = canvas.height,
+            context = canvas.getContext('2d');
+            debugger;
+        context.clearRect(0, 0, width, height);
+        var step = Math.ceil(data.length / width);
+        var amp = height / 2;
+        for (var i = 0; i < width; i++) {
+            var min = 1.0;
+            var max = -1.0;
+            for (var j = 0; j < step; j++) {
+                var datum = data[(i * step) + j];
+                if (datum < min)
+                    min = datum;
+                if (datum > max)
+                    max = datum;
+            }
+            context.fillRect(i, (1 + min) * amp, 1, Math.max(1, (max - min) * amp));
+        }
+    });
+  }
+
+  startBroadcast() {
+    if (this.state.selectedSource) {
+      this.setState({
+        disabled: true
+      });
+      this.bc.start(this.state.selectedSource);
+    }
   }
 
   stopBroadcast() {
     this.setState({
-      disabled:false
+      disabled: false
+    })
+    this.bc.stop();
+  }
+
+  sourceInput(event, index, value) {
+    this.setState({
+      selectedSource: value
     })
   }
 
@@ -63,6 +119,14 @@ class BroadcastLive extends React.Component {
   }
 
   render() {
+    console.log(this.state.audioSources);
+    var dropDown = (
+      <DropDownMenu value={this.state.selectedSource} onChange={this.sourceInput.bind(this)}>
+        <MenuItem value={null} key={null} primaryText={"Select a source..."} />
+        {this.state.audioSources.map(source => <MenuItem value={source.id} key={source.id} primaryText={source.label} />)}
+      </DropDownMenu>
+    )
+
     return (
       <div style={styles.container}>
         <NavBar history={this.history}/>
@@ -86,6 +150,7 @@ class BroadcastLive extends React.Component {
               <FloatingActionButton onClick={this.stopBroadcast.bind(this)} disabled={!this.state.disabled}>
                <MicOff />
               </FloatingActionButton>
+              {dropDown}
             </CardActions>
 
             <CardTitle title={this.state.name}/>
@@ -99,6 +164,9 @@ class BroadcastLive extends React.Component {
                 <div><span style={styles.count}>{this.state.listnerLiveCount.toLocaleString()}</span><span> Listens <Face /></span></div>
                 <div><span style={styles.count}>{this.state.heartCount.toLocaleString()}</span><span> Hearts <Favorite color={Colors.red500}/></span></div>
             </CardText>
+            <div id="canvas-container">
+              <canvas width="600" height="100" id="canvas"></canvas>
+            </div>
           </Card>
 
         </div>
