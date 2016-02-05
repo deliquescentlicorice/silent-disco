0b870500ffc44425a9f5f1c715639c358d32d809
Joshua Wong <Josh@joshua-wongs-macbook-pro.local>
2016-02-04 23:11:57 -0800

    Updated styling of buttons and forms

--- webApp/src/Components/Login.js
+++ webApp/src/Components/Login.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { History } from 'react-router';
 import reactMixin from 'react-mixin';
-import $ from '../../public/js/jquery-1.11.1.min';
+
+// MATERIAL DESIGN
+import Card from '../../node_modules/material-ui/lib/card/card';
 import RaisedButton from '../../node_modules/material-ui/lib/raised-button';
+import FontIcon from '../../node_modules/material-ui/lib/font-icon';
+import CardText from '../../node_modules/material-ui/lib/card/card-text';
+
 import NavBar from './NavBar';
 import Auth from '../utils/Auth';
+
 
 class Login extends React.Component {
 
@@ -26,11 +32,43 @@ class Login extends React.Component {
       <div>
 
       <NavBar title="Login" history={this.history}/>
-      <RaisedButton primary={true} onClick={this.login.bind(this)} label="Login With SoundCloud"/>
+      <Card style={styles.cardContainer}>
+        <CardText>
+
+        </CardText>
+        <RaisedButton 
+          style={styles.mainBox}
+          primary={true} 
+          onClick={this.login.bind(this)} 
+          label="Login With SoundCloud"
+          icon={<FontIcon className="fa fa-soundcloud"/>}
+        />
+        <CardText>
+          
+        </CardText>
+      </Card>
       
       </div>
     )
   }
+}
+
+var styles = {
+
+
+  cardContainer:{
+    'display': 'flex',
+    'flexDirection':'row',
+    'flexWrap': 'wrap',
+    alignItem:'center',
+    justifyContent:'center'
+  },
+
+
+  mainBox: {
+    'flexGrow':1,
+  },
+
 }
 
 reactMixin.onClass(Login, History);
