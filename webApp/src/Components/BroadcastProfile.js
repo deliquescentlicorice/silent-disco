5d28e72ee4d9fe2307fa7a7a26f9c95b16bb3bd6
Steven Law <slaw@nuclearanvil.com>
2016-02-05 16:34:50 -0800

    Refactored presentation into separate component.

--- webApp/src/Components/BroadcastProfile.js
+++ webApp/src/Components/BroadcastProfile.js
@@ -1,121 +1,61 @@
 import React from 'react';
 
-// MATERIAL DESIGN CARD
-import Card from 'material-ui/lib/card/card';
-import CardActions from '../../node_modules/material-ui/lib/card/card-actions';
-import CardHeader from '../../node_modules/material-ui/lib/card/card-header';
-import CardMedia from '../../node_modules/material-ui/lib/card/card-media';
-import CardTitle from '../../node_modules/material-ui/lib/card/card-title';
-import FlatButton from '../../node_modules/material-ui/lib/flat-button';
-import FloatingActionButton from '../../node_modules/material-ui/lib/floating-action-button';
-import TrendingUp from '../../node_modules/material-ui/lib/svg-icons/action/trending-up';
-import Favorite from '../../node_modules/material-ui/lib/svg-icons/action/favorite';
-import Face from '../../node_modules/material-ui/lib/svg-icons/action/face';
-import CardText from '../../node_modules/material-ui/lib/card/card-text';
-import TextField from '../../node_modules/material-ui/lib/text-field';
-import RaisedButton from '../../node_modules/material-ui/lib/raised-button';
-import Colors from '../../node_modules/material-ui/lib/styles/colors';
-
-import Paper from '../../node_modules/material-ui/lib/paper';
-import List from '../../node_modules/material-ui/lib/lists/list';
-
 // ROUTER
 import { History } from 'react-router';
 import reactMixin from 'react-mixin';
 
 // COMPONENTS
 import NavBar from './NavBar.js'
-import BroadcastEntry from './BroadcastEntry.js'
+import BroadcastProfileView from './BroadcastProfileView.js'
+import Loading from './Loading.js'
 
 // AJAX GET CALL
 import $ from '../../public/js/jquery-1.11.1.min';
-
 
 class BroadcastProfile extends React.Component {
   constructor() {
     super();
 
     this.state = {
+      user: {},
+      isLoading: true
     };
   }
 
   componentDidMount(){
     this.fetchUserData()
-    
   }
 
   fetchUserData() {
+    this.setState({
+      isLoading: true
+    })
     $.ajax({
       url: '/api/user/'+ this.props.params.userId
     })
-    .done((responseData) => {
+    .done((userData) => {
       this.setState({
-        user: responseData
+        user: userData,
+        isLoading: false
       });
-      console.log('State',this.state);
     });
-  }
-
-  goToBroadcast() {
-    console.log("broadcast",this)
-    var broadcastId= this.props.details._id;
-    console.log("Going To Stream " + broadcastId);
-
-    this.props.history.push({
-      pathname: '/broadcast/'+broadcastId
-
-      //so we get which song from the state; a single GET request
-      //we could also send a new request to the server to get each individual song
-        //i'll look at this for testing purposes
-      // state: { stream : this.props.state.streams[broadcastId] }
-    });
-  }
-
-  renderStream(key){
-    return <BroadcastEntry goToBroadcast={this.goToBroadcast} history={this.history} state={this.state} key={key} index={key} details={this.state.user.streams[key]} />
-  }
-
-  listnerCountTotal(){
-    var sum = 0;
-    for(var i=0;i<this.state.user.streams.length;i++){
-      sum = sum + this.state.user.streams[i].listenerMaxCount;
-    }
-    return sum.toLocaleString()
-  }
-
-  heartCountTotal(){
-    var sum = 0;
-    for(var i=0;i<this.state.user.streams.length;i++){
-      sum = sum + this.state.user.streams[i].heartCountNum;
-    }
-    return sum.toLocaleString()
   }
 
   render() {
+    var partial;
+    if (this.state.isLoading) {
+      partial = <Loading />
+    } else {
+      partial = <BroadcastProfileView
+        user={this.state.user.user}
+        streams={this.state.user.streams}
+        history={this.history}
+        isLoading={this.state.isLoading} />
+    }
     return (
       <div style={styles.container}>
         <NavBar title="Profile" history={this.history}/>
-        <div style={styles.box}>
-          <Card>
-          <CardHeader
-              title={this.state.user.user.scUsername}
-              subtitle={this.state.user.user.full_name}
-              avatar={this.state.user.user.scAvatarUri}
-            />
-            <div>
-              <CardText>
-                <h2>TOTAL STATS <TrendingUp /></h2>
-                <div><span style={styles.count}>{this.listnerCountTotal()}</span><span> Listens <Face /></span></div>
-                <div><span style={styles.count}>{this.heartCountTotal()}</span><span> Hearts <Favorite color={Colors.red500}/></span></div>
-              </CardText>
-            </div>
-          </Card>
-        </div>
-        <div style={styles.box}>
-          <List subheader="Previous Streams" style={styles.box}>
-            {Object.keys(this.state.user.streams).map(this.renderStream.bind(this))}
-          </List>
-        </div>
+        {partial}
       </div>
     )
   } 
@@ -125,20 +65,6 @@ var styles = {
   container:{
     'display': 'flex',
     'flexDirection' :'column',
-  },
-
-  cardContainer:{
-    'display': 'flex',
-    'flexDirection':'row',
-    'flexWrap': 'wrap'
-  },
-
-  box: {
-    flex:1
-  },
-
-  count:{
-    fontWeight: 'bold'
   }
 }
 
