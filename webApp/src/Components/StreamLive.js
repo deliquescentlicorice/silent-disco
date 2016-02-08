571a3d017cbfe38d5f8db46331f9594dcee4b81e
Steven Law <slaw@nuclearanvil.com>
2016-02-08 12:02:46 -0800

    Moved presentation bits to separate component

--- webApp/src/Components/StreamLive.js
+++ webApp/src/Components/StreamLive.js
@@ -1,30 +1,13 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
 
 // COMPONENTS
 import NavBar from './NavBar.js';
 import Loading from './Loading.js'
-import BroadcastStats from './BroadcastStats';
+import StreamLiveView from './StreamLiveView.js';
 
 // MEDIA PLAYER
 import Sound from '../../node_modules/react-sound';
 
-//  RESPONSIVE CSS
-import MediaQuery from '../../node_modules/react-responsive';
-
-// MATERIAL DESIGN
-import Card from '../../node_modules/material-ui/lib/card/card';
-import CardActions from '../../node_modules/material-ui/lib/card/card-actions';
-import CardMedia from '../../node_modules/material-ui/lib/card/card-media';
-import CardText from '../../node_modules/material-ui/lib/card/card-text';
-import CardTitle from '../../node_modules/material-ui/lib/card/card-title';
-import FloatingActionButton from '../../node_modules/material-ui/lib/floating-action-button';
-import Play from '../../node_modules/material-ui/lib/svg-icons/av/play-arrow';
-import Pause from '../../node_modules/material-ui/lib/svg-icons/av/pause';
-import Favorite from '../../node_modules/material-ui/lib/svg-icons/action/favorite';
-import AppBar from '../../node_modules/material-ui/lib/app-bar';
-// import CardHeader from '../../node_modules/material-ui/lib/card/card-header';
-// import CardText from '../../node_modules/material-ui/lib/card/card-text';
 import $ from '../../public/js/jquery-1.11.1.min';
 
 var BASE_URL = 'http://' + document.location.host;
@@ -40,7 +23,6 @@ class StreamLive extends React.Component {
       desc: "",
       name: "",
       broadcaster: "",
-      description: "",
       image: "",
       listenerLiveCount: "",
       listenerMaxCount: "",
@@ -50,11 +32,12 @@ class StreamLive extends React.Component {
       image: "",
       soundcloud: "",
       website: "",
-      websiteTitle: ""
+      websiteTitle: "",
+      streamId: this.props.params.streamId || window.location.pathname.substr(8)
     }
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this.fetchStreamData();
   }
 
@@ -75,18 +58,9 @@ class StreamLive extends React.Component {
   }
 
   addHeart() {
-    var PUT_HEART = BASE_URL + '/api/stream/' + this.props.params.streamId;
+    var PUT_HEART = BASE_URL + '/api/stream/' + this.state.streamId;
 
-    // fetch(PUT_HEART, {
-    //   method: 'PUT',
-    //   body: JSON.stringify()
-    // })
-    // .then((response) => response.json())
-    // .then((responseData) => {
-    //   this.setState({
-    //     heartCount: responseData.heartCountNum.length > 5 ? '> 9 999' : responseData.heartCountNum
-    //   });
-    // })
+    // As of iOS 9.3 and OSX 10.11, Safari does not support fetch
     $.ajax({
         url: PUT_HEART,
         method: 'PUT',
@@ -102,7 +76,7 @@ class StreamLive extends React.Component {
 
   fetchStreamData() {
     $.ajax({
-      url: BASE_URL + '/api/stream/' + this.props.params.streamId
+      url: BASE_URL + '/api/stream/' + this.state.streamId
     })
     .done((streamData) => {
       $.ajax({
@@ -136,34 +110,11 @@ class StreamLive extends React.Component {
     )
 
     if (!this.state.isLoading) {
-      partial = (
-        <div style={styles.cardContainer}>
-          <Card style={styles.mainBox}>
-            <CardMedia style={styles.streamImage} >
-              <img src={this.state.image} />
-            </CardMedia>
-            <CardTitle title={this.state.name} subtitle={linkBar} />
-            <CardText>
-              {this.state.desc}
-            </CardText>
-            <CardActions>
-              <FloatingActionButton onClick={this.playSong.bind(this)} secondary={true} disabled={this.state.disabled}>
-                <Play />
-              </FloatingActionButton>
-              <span></span>
-              <FloatingActionButton onClick={this.stopSong.bind(this)} secondary={true}  disabled={!this.state.disabled}>
-                <Pause />
-              </FloatingActionButton>
-              <FloatingActionButton onClick={this.addHeart.bind(this)}>
-                <Favorite />
-              </FloatingActionButton>
-            </CardActions>
-          </Card>
-          <Card style={styles.box}>
-            <BroadcastStats listenerLiveCount={this.state.listenerLiveCount} listenerMaxCount={this.state.listenerMaxCount} heart={this.state.heartCount}/>
-          </Card>
-        </div>
-      )
+      partial = <StreamLiveView
+        state={this.state}
+        playSong={this.playSong.bind(this)}
+        stopSong={this.stopSong.bind(this)}
+        addHeart={this.addHeart.bind(this)} />
     }
     return (
       <div>
@@ -173,7 +124,7 @@ class StreamLive extends React.Component {
         </div>
         
         <Sound
-          url={'/stream/' + this.props.params.streamId}
+          url={'/stream/' + this.state.streamId}
           playStatus={this.state.status}
           onLoading={this.handleSongLoading}
           onPlaying={this.handleSongPlaying}
@@ -184,36 +135,9 @@ class StreamLive extends React.Component {
 }
 
 var styles = {
-  container:{
+  container: {
     'display': 'flex',
     'flexDirection' :'column',
-  },
-  cardContainer:{
-    'display': 'flex',
-    'flexDirection':'row',
-    'flexWrap': 'wrap'
-  },
-
-  box: {
-    'flex':3
-  },
-
-  mainBox: {
-    'flexGrow':1,
-    'maxWidth': '600px',
-    'alignContent': 'center',
-  },
-
-  streamImage:{
-    'maxWidth': '300px'
-  },
-
-  card: {
-    'flexGrow':1,
-    'alignContent': 'center',
-    'maxWidth': 325
-    // 'border': '10px solid yellow',
-    // 'height': '100vh'
   }
 }
 
