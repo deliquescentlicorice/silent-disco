15ea4f4d49f16d489c3f9fea5beb8fb62e51bb50
Steven Law <slaw@nuclearanvil.com>
2016-02-08 16:12:35 -0800

    Moved routes to routes.js or removed them as appropriate.

--- webApp/server/server.js
+++ webApp/server/server.js
@@ -1,25 +1,11 @@
 /**
  * Created on 1/28/16
  */
+
+// general dependencies
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-
-
-var MongoClient = require('mongodb');
-var mongoose = require('mongoose');
-var Admin = mongoose.mongo.Admin;
-
-var localMongo = 'mongodb://localhost:27017/silentdisco';
-mongoose.connect(localMongo);
-
-var app = express();
-var port = process.env.PORT || 3000;
-
-var encoder = require('./controllers/encoder');
-var binaryServer = require('binaryjs').BinaryServer;
-var binarySocketHandler = require('./binarySockets.js');
-
 var scAuth = require('./config/scAuth.js');
 var passport = require('passport');
 var cookieParser = require('cookie-parser');
@@ -27,8 +13,21 @@ var session = require('express-session');
 var SoundCloudStrategy = require('passport-soundcloud').Strategy;
 var apiKeys = require('./config/apiKeys');
 
-var version = process.env.version || 'DEV';
+// database
+var MongoClient = require('mongodb');
+var mongoose = require('mongoose');
+var Admin = mongoose.mongo.Admin;
+var localMongo = 'mongodb://localhost:27017/silentdisco';
+mongoose.connect(localMongo);
 
+// server setup
+var app = express();
+var port = process.env.PORT || 3000;
+var binaryServer = require('binaryjs').BinaryServer;
+var binarySocketHandler = require('./binarySockets.js');
+
+// dev stuff
+var version = process.env.version || 'DEV';
 if (version === 'DEV') {
   var webpack = require('webpack');
   var config = require('../webpack.config.dev');
@@ -46,6 +45,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
+// auth + session
 app.use(cookieParser());
 app.use(session({ secret: 'disco' , resave: true, saveUninitialized: false}));
 app.use(passport.initialize());
@@ -53,36 +54,9 @@ app.use(passport.session());
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
-
 passport.deserializeUser(function(obj, done) {
   done(null, obj);
 });
-
-require('./routes.js')(app, express, scAuth.ensureAuth);
-
-app.use(express.static(__dirname + '/../src'));
-
-//broadcasting client
-app.use("/broadcast", express.static(__dirname + '/../public'));
-
-//listen API route
-app.get('/stream/:id', encoder.listenHandler);
-
-app.get('*', function(req, res) {
-  console.log('hitting the catchall route');
-  res.sendFile(path.join(__dirname + '/../src', 'index.html'));
-});
-
-var server = app.listen(port);
-
-console.log('Listening on port:' + port);
-
-var bServer = binaryServer({
-  server: server
-});
-
-bServer.on('connection', binarySocketHandler.connect);
-
 passport.use(new SoundCloudStrategy({
   clientID: apiKeys.clientID,
   clientSecret: apiKeys.clientSecret,
@@ -92,18 +66,19 @@ passport.use(new SoundCloudStrategy({
     console.log('done', result);
     return done(err, result);
   });
-  // controller.isUserInDb([unique identifier goes here], function (inDb){
-  //   if(inDb){
-  //     scAuth.login({profile: profile}, function (err, profile){
-  //       return done(err, profile);
-  //     });
-  //   } else {
-  //     scAuth.signup({profile: profile}, function (err, profile){
-  //       return done(err, profile);
-  //     });
-  //   }
-  // });
   }
 ));
+
+// routes
+app.use(express.static(__dirname + '/../src'));
+require('./routes.js')(app, express, scAuth.ensureAuth);
+
+// server startup
+var server = app.listen(port);
+console.log('Listening on port:' + port);
+var bServer = binaryServer({
+  server: server
+});
+bServer.on('connection', binarySocketHandler.connect);
 
 module.exports = app;
