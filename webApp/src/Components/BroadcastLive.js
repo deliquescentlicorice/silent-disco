2315a8bf2513069f990bcb9ba709ca1781440ac9
Steven Law <slaw@nuclearanvil.com>
2016-02-09 19:19:03 -0800

    Refactored out presentation portions of BroadcastLive into separate component.

--- webApp/src/Components/BroadcastLive.js
+++ webApp/src/Components/BroadcastLive.js
@@ -1,57 +1,22 @@
 import React from 'react';
 
-// MATERIAL DESIGN
-import Card from 'material-ui/lib/card/card';
-import CardActions from '../../node_modules/material-ui/lib/card/card-actions';
-import CardHeader from '../../node_modules/material-ui/lib/card/card-header';
-import CardMedia from '../../node_modules/material-ui/lib/card/card-media';
-import CardTitle from '../../node_modules/material-ui/lib/card/card-title';
-import DropDownMenu from '../../node_modules/material-ui/lib/DropDownMenu';
-import FlatButton from '../../node_modules/material-ui/lib/flat-button';
-import FloatingActionButton from '../../node_modules/material-ui/lib/floating-action-button';
-import Favorite from '../../node_modules/material-ui/lib/svg-icons/action/favorite';
-import Face from '../../node_modules/material-ui/lib/svg-icons/action/face';
-import TrendingUp from '../../node_modules/material-ui/lib/svg-icons/action/trending-up';
-import MenuItem from '../../node_modules/material-ui/lib/menus/menu-item';
-import Mic from '../../node_modules/material-ui/lib/svg-icons/av/mic';
-import MicOff from '../../node_modules/material-ui/lib/svg-icons/av/mic-off';
-import CardText from '../../node_modules/material-ui/lib/card/card-text';
-import TextField from '../../node_modules/material-ui/lib/text-field';
-import RaisedButton from '../../node_modules/material-ui/lib/raised-button';
-import Colors from '../../node_modules/material-ui/lib/styles/colors';
-import Tabs from '../../node_modules/material-ui/lib/tabs/tabs';
-import Tab from '../../node_modules/material-ui/lib/tabs/tab';
-
-
-import Paper from '../../node_modules/material-ui/lib/paper';
-
-import ListItem from '../../node_modules/material-ui/lib/lists/list-item';
-import Avatar from '../../node_modules/material-ui/lib/avatar';
-import PlayCircleOutline from 'material-ui/lib/svg-icons/av/play-circle-outline';
-import List from '../../node_modules/material-ui/lib/lists/list';
-
 // ROUTER
 import { History } from 'react-router';
 import reactMixin from 'react-mixin';
 
 // COMPONENTS
 import NavBar from './NavBar.js';
+import BroadcastLiveView from './BroadcastLiveView.js';
 import Loading from './Loading.js';
-import BroadcastStats from './BroadcastStats.js';
-import BroadcastAUX from './BroadcastAUX.js';
-import BroadcastSCEntry from './BroadcastSCEntry.js';
-import BroadcastQueueEntry from './BroadcastQueueEntry.js';
-import BroadcastAudioPlayer from './BroadcastAudioPlayer.js';
 
 // VISUALIZER
 import visualizer from './visualizer.js';
 
+// CLIENTID
+import SC_Client from '../../server/config/apiKeys';
+
 // AJAX GET CALL
 import $ from '../../public/js/jquery-1.11.1.min';
-
-//CLIENTID
-import SC_Client from '../../server/config/apiKeys'
-
 
 class BroadcastLive extends React.Component {
   constructor(props) {
@@ -86,10 +51,8 @@ class BroadcastLive extends React.Component {
     };
   }
 
-
   gotMediaDevices(devices) {
     var audioSources = [];
-    console.log('GMD was called');
     devices.forEach(function(device) {
       if (device.kind === 'audio') {
         audioSources.push({
@@ -104,25 +67,13 @@ class BroadcastLive extends React.Component {
   }
 
   componentWillMount() {
-    console.log(this.props.params.streamId);
     this.bc = new Broadcaster(this.props.params.streamId, this.gotMediaDevices.bind(this), visualizer);
     this.fetchData();
-  }
-
-
-  componentDidMount() {
-    // document.getElementById("soundcloudPlayer").addEventListener('ended', this.handleMediaEnd.bind(this))
-    // if(this.refs.soundcloudPlayer){
-    //   this.refs.soundcloudPlayer.addEventListener('ended', this.handleMediaEnd.bind(this))
-    // }
-
   }
 
   componentWillUnmount() {
     this.bc.stop();
   }
-
-
 
   fetchData() {
     $.ajax({
@@ -142,14 +93,14 @@ class BroadcastLive extends React.Component {
         playing: stream.playing,
         favorites: fav
       });
-
     });
   }
-  startHTMLBroadcast(){
+
+  startHTMLBroadcast() {
     this.bc.startFromHTML("soundcloudPlayer");
   }
 
-  stopHTMLBroadcast(){
+  stopHTMLBroadcast() {
     this.bc.stop();
   }
 
@@ -165,18 +116,16 @@ class BroadcastLive extends React.Component {
   stopBroadcast() {
     this.setState({
       disabled: false
-    })
+    });
     this.bc.stop();
   }
 
   handleMediaEnd() {
-    let newQueue = this.state.songQueue
-    let endedQueue = this.state.endedSongQueue
-    console.log("newQueue", newQueue)
-    if(newQueue.length===1){
-      let playedSong = newQueue.shift()
-      console.log("shifted queue", newQueue)
-      endedQueue.push(playedSong)
+    let newQueue = this.state.songQueue;
+    let endedQueue = this.state.endedSongQueue;
+    if (newQueue.length === 1) {
+      let playedSong = newQueue.shift();
+      endedQueue.push(playedSong);
       this.setState({
         songQueue: newQueue,
         currentSong: {
@@ -188,63 +137,48 @@ class BroadcastLive extends React.Component {
           }
         },
         endedSongQueue: endedQueue
-      })
-      console.log(endedQueue)
-    } else if(newQueue.length>1){
-      let playedSong = newQueue.shift()
-      console.log("shifted queue", newQueue)
-      endedQueue.push(playedSong)
+      });
+    } else if (newQueue.length>1) {
+      let playedSong = newQueue.shift();
+      endedQueue.push(playedSong);
       this.setState({
         songQueue: newQueue,
         currentSong: newQueue[0],
         endedSongQueue: endedQueue
-      })
-      console.log(endedQueue)
+      });
     } 
   }
 
   sourceInput(event, index, value) {
-    console.log("value",value)
     this.setState({
       selectedSource: value
-    })
-    console.log("changed state",this.state)
+    });
   }
 
   goToProfile() {
     this.props.history.push({
       pathname: '/user/'+ this.state.creator
-    })
+    });
   }
 
-  changeSCSong(selectedSong){
-    console.log("currentSong", this)
-    console.log("selectedSong", selectedSong)
-    let newQueue = this.state.songQueue
-    newQueue.push(selectedSong)
-    if(newQueue.length===1){
+  addSongToQueue(selectedSong) {
+    let newQueue = this.state.songQueue;
+    newQueue.push(selectedSong);
+    if (newQueue.length === 1) {
       this.setState({
         songQueue: newQueue,
         currentSong: newQueue[0]
-      })
+      });
     } else {
       this.setState({
         songQueue: newQueue
-      })
+      });
     }
-    
-    console.log(this.state)
-  }
-
-  searchInput(event, index, value) {
-    this.setState({
-      search: event.target.value
-    });
   }
 
   submitSearch(event) {
     //get the value of search
-    let search = this.state.search
+    let search = this.state.search;
 
     //query the soundcloud database
     SC.initialize({
@@ -262,176 +196,46 @@ class BroadcastLive extends React.Component {
         search: "",
         searchResults:tracks.collection,
         nextSearch:tracks.next_href
-      })
-      
+      });
     });
   }
 
-  loadMoreSongs(){
-    let query = this.state.nextSearch
-
+  loadMoreSongs() {
+    let query = this.state.nextSearch;
     $.get(query).then((tracks) => {
       // page through results, 100 at a time
-      console.log('track results', tracks)
       this.setState({
         search: "",
         searchResults:tracks.collection,
         nextSearch:tracks.next_href || ''
-      })
-      
+      });
     });
   }
 
-  removeSongFromQueue(song){
-    let newQueue = this.state.songQueue
-    newQueue.splice(song,1)
+  removeSongFromQueue(song) {
+    let newQueue = this.state.songQueue;
+    newQueue.splice(song,1);
     this.setState({
       songQueue: newQueue
-    })
-    
-  }
-
-  renderSCEntry(index, key){
-    return <BroadcastSCEntry 
-      history={this.history} 
-      changeSCSong={this.changeSCSong.bind(this)} 
-      state={this.state} 
-      key={key} 
-      index={index} 
-      title={index.title} 
-      genre={index.genre} 
-      stream_url={index.stream_url} 
-      artwork={index.artwork_url || index.user.avatar_url} 
-    />
-  }
-
-  renderQueueEntry(index, key){
-    return <BroadcastQueueEntry 
-      history={this.history} 
-      removeSongFromQueue={this.removeSongFromQueue.bind(this)} 
-      state={this.state} 
-      key={key} 
-      index={index} 
-      title={index.title} 
-      genre={index.genre} 
-      stream_url={index.stream_url} 
-      artwork={index.artwork_url || index.user.avatar_url} 
-    />
+    });
   }
 
   render() {
-
-    var dropDown = (
-      <DropDownMenu value={this.state.selectedSource} onChange={this.sourceInput.bind(this)}>
-        <MenuItem value={null} key={null} primaryText={"Select a source..."} />
-        {this.state.audioSources.map(source => <MenuItem value={source.id} key={source.id} primaryText={source.label} />)}
-      </DropDownMenu>
-    )
-
     var partial = <Loading />
     if (!this.state.isLoading) {
-      partial = (
-        <div>
-          <div style={styles.cardContainer}>
-            <Card style={styles.mainBox}>
-              <a href="#">
-              <CardHeader
-                onClick={this.goToProfile.bind(this)}
-                title={this.state.artistAlias}
-                subtitle={this.state.artist}
-                avatar={this.state.artistImage}
-              /></a>
-              <CardMedia style={styles.streamImage}>
-                <img src={this.state.artistImage}/>
-              </CardMedia>
-          
-
-              <CardActions>
-                <FloatingActionButton onClick={this.startBroadcast.bind(this)} disabled={this.disabled}>
-                  <Mic />
-                </FloatingActionButton>
-                <FloatingActionButton onClick={this.stopBroadcast.bind(this)} disabled={!this.disabled}>
-                 <MicOff />
-                </FloatingActionButton>
-                {dropDown}
-              </CardActions>
-
-              <CardTitle title={this.state.name}/>
-              <CardText>
-                {this.state.description}
-              </CardText>
-            </Card>
-            <Card style={styles.box}>
-              <BroadcastStats 
-                listenerLiveCount={this.state.listenerLiveCount} 
-                listenerMaxCount={this.state.listenerMaxCount} 
-                heart={this.state.heartCount}/>
-              <canvas width="600" height="100" id="visualizer"></canvas>
-            </Card>
-          </div>
-          <div style={styles.cardContainer}>
-            <Card style={styles.box}>
-              <CardTitle title="Soundcloud Setlist"/>
-              <List subheader="Now Playing">
-                <ListItem
-                  primaryText={this.state.currentSong.title}
-                  secondaryText={this.state.currentSong.genre}
-                  leftAvatar={<Avatar src={this.state.currentSong.artwork_url || this.state.currentSong.user.avatar_url} />}
-                  disabled={true}
-                />
-              </List>
-              <br/>
-              <BroadcastAudioPlayer
-                src={this.state.currentSong.stream_url + '?client_id=' + SC_Client.clientID } 
-                handleMediaEnd = {this.handleMediaEnd.bind(this)}
-              />
-
-              <br/>
-              <br/>
-              <FloatingActionButton onClick={this.startHTMLBroadcast.bind(this)}>
-                <Mic />
-              </FloatingActionButton>
-              <FloatingActionButton onClick={this.stopHTMLBroadcast.bind(this)} >
-               <MicOff />
-              </FloatingActionButton><br/><br/>
-              <Tabs>
-                <Tab label="Playlist">
-                  <List subheader="Up Next">
-                    {this.state.songQueue.map(this.renderQueueEntry.bind(this))}
-                  </List>
-                  <List subheader="Played">
-                    {this.state.endedSongQueue.map(this.renderQueueEntry.bind(this))}
-                  </List>
-                </Tab>
-                <Tab label="Favorites">
-                  <List subheader="Select Songs for the Queue">
-                    {this.state.favorites.map(this.renderSCEntry.bind(this))}
-                  </List>
-                </Tab>
-                <Tab label="Search Soundcloud">
-                  <TextField
-                    onChange={this.searchInput.bind(this)}
-                    hintText="Search Soundcloud"
-                    floatingLabelText="Search Soundcloud"
-                  /> 
-                  <FlatButton
-                    onClick={this.submitSearch.bind(this)}
-                    label="Search"
-                    primary={true} 
-                  />
-                  {this.state.searchResults.map(this.renderSCEntry.bind(this))}
-                    <FlatButton
-                      onClick={this.loadMoreSongs.bind(this)}
-                      label="More Songs"
-                      primary={true} 
-                    />
-                    
-                </Tab>
-              </Tabs>
-            </Card>
-          </div>
-        </div>
-      )
+      partial = <BroadcastLiveView
+        addSongToQueue={this.addSongToQueue.bind(this)}
+        removeSongFromQueue={this.removeSongFromQueue.bind(this)}
+        sourceInput={this.sourceInput.bind(this)}
+        goProfile={this.goToProfile.bind(this)}
+        startBroadcast={this.startBroadcast.bind(this)}
+        stopBroadcast={this.stopBroadcast.bind(this)}
+        handleMediaEnd={this.handleMediaEnd.bind(this)}
+        startHTMLBroadcast={this.startHTMLBroadcast.bind(this)}
+        stopHTMLBroadcast={this.stopHTMLBroadcast.bind(this)}
+        submitSearch={this.submitSearch.bind(this)}
+        loadMoreSongs={this.loadMoreSongs.bind(this)}
+        state={this.state} />
     }
     return (
       <div style={styles.container}>
@@ -446,50 +250,9 @@ var styles = {
   container:{
     'display': 'flex',
     'flexDirection' :'column',
-  },
-
-  cardContainer:{
-    'display': 'flex',
-    'flexDirection':'row',
-    'flexWrap': 'wrap'
-  },
-
-  box: {
-    'flex':3
-  },
-
-  mainBox: {
-    'flexGrow':1,
-    'maxWidth': '600px'
-  },
-
-  streamImage:{
-    'maxWidth': '300px'
-  },
-
-  count:{
-    fontWeight: 'bold'
   }
 }
 
 reactMixin.onClass(BroadcastLive, History);
 
 export default BroadcastLive;
-
-// var dropDown = (
-//       <DropDownMenu value={this.state.selectedSource} onChange={this.sourceInput.bind(this)}>
-//         <MenuItem value={null} key={null} primaryText={"Select a source..."} />
-//         {this.state.audioSources.map(source => <MenuItem value={source.id} key={source.id} primaryText={source.label} />)}
-//       </DropDownMenu>
-//     )
-
-// <BroadcastAUX 
-              // startBroadcast={this.startBroadcast}
-              // stopBroadcast={this.stopBroadcast}
-              // disabled={this.state.disabled}
-              // selectedSource={this.state.selectedSource}
-              // sourceInput={this.sourceInput}
-              // audioSources={this.state.audioSources}
-            // />
-
-
