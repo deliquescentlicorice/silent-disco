2f6ffcc503f49619ae27d86bb829c3d052b17e79
radelmann <robadelmann@gmail.com>
2016-02-10 14:45:44 -0800

    real-time db updates for stream start and stop

--- webApp/server/binarySockets.js
+++ webApp/server/binarySockets.js
@@ -1,5 +1,6 @@
 var encoder = require('./controllers/encoder');
 var binaryServer = require('binaryjs').BinaryServer;
+var streamsController = require('./controllers/streamsController');
 
 module.exports = function(server) {
   var bServer = binaryServer({
@@ -10,20 +11,25 @@ module.exports = function(server) {
     console.log("new binary socket connection...");
 
     //todo handle events
-
     client.on('stream', function(stream, meta) {
       if (meta.type === 'stream') {
         console.log('broadcast start');
         console.log(meta);
         console.log("Stream start...@" + meta.sampleRate + "Hz");
         console.log("Stream Id:" + meta.streamId);
-
-        //support for multiple streams  
+        
+        //support multiple streams  
         var streamId = meta.streamId;
-
-        //handle listeners arriving to stream before it starts broadcasting
         encoder.stdin[streamId] = stream;
 
+        //update database stream model as started
+        streamsController.streamWasStartedOrStopped(streamId, 'started', function(err, results) {
+          if (err) {
+            console.log(err);
+          }
+        });
+
+        //handle listeners arriving to stream before it starts broadcasting
         //if there is a queue of clients waiting for this stream to start
         if (encoder.streamQueue[streamId]) {
           encoder.streamQueue[streamId].forEach(function(req) {
@@ -34,11 +40,18 @@ module.exports = function(server) {
 
         stream.on('end', function() {
           console.log('broadcast end');
+          //update database stream model as stopped
+          streamsController.streamWasStartedOrStopped(streamId, 'stopped', function(err, results) {
+            if (err) {
+              console.log(err);
+            }
+          });
           encoder.stdin[streamId].end();
           delete encoder.stdin[streamId];
         });
       } else {
         console.log(meta);
+        //broadcast event to 
         emit(meta);
       }
     });
