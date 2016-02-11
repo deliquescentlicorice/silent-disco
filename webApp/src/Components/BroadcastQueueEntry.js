8a4e75364d5195aa6a2a931c25bbb5374aa2ebf5
Steven Law <slaw@nuclearanvil.com>
2016-02-10 16:43:55 -0800

    Removed unnecessary imports.

--- webApp/src/Components/BroadcastQueueEntry.js
+++ webApp/src/Components/BroadcastQueueEntry.js
@@ -1,15 +1,10 @@
 import React from 'react';
+
+// MATERIAL DESIGN
 import ListItem from '../../node_modules/material-ui/lib/lists/list-item';
 import Divider from '../../node_modules/material-ui/lib/divider';
 import Avatar from '../../node_modules/material-ui/lib/avatar';
-import IconButton from '../../node_modules/material-ui/lib/icon-button';
-import MoreVertIcon from '../../node_modules/material-ui/lib/svg-icons/navigation/more-vert';
-import IconMenu from '../../node_modules/material-ui/lib/menus/icon-menu';
-import MenuItem from '../../node_modules/material-ui/lib/menus/menu-item';
-import Favorite from '../../node_modules/material-ui/lib/svg-icons/action/favorite';
-import Face from '../../node_modules/material-ui/lib/svg-icons/action/face';
 import Close from '../../node_modules/material-ui/lib/svg-icons/navigation/close';
-import Colors from '../../node_modules/material-ui/lib/styles/colors';
 
 class BroadcastQueueEntry extends React.Component {
   constructor(props){
