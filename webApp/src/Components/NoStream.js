e4dd473f94f3a051d8b07d0fc18f89db4883a2f8
Joshua Wong <Josh@joshua-wongs-macbook-pro.local>
2016-02-11 23:22:27 -0800

    Styling of No Stream Page

--- webApp/src/Components/NoStream.js
+++ webApp/src/Components/NoStream.js
@@ -9,7 +9,7 @@ import List from '../../node_modules/material-ui/lib/lists/list';
 import Card from '../../node_modules/material-ui/lib/card/card';
 import CardText from '../../node_modules/material-ui/lib/card/card-text';
 import CardMedia from '../../node_modules/material-ui/lib/card/card-media';
-
+import FlatButton from '../../node_modules/material-ui/lib/flat-button';
 
 
 class NoStream extends React.Component {
@@ -20,18 +20,39 @@ class NoStream extends React.Component {
 
   render() {
     return (
-      <Card>
-        <CardText>
+      <div>
+        <h1 style={styles.titleFontBlack}>
           No Streams Are Currently Available
-        </CardText>
-      </Card>
+        </h1>
+        <div style={styles.flexColumn}>
+          <div style={styles.boxCenter}>
+            <a href='/broadcast/setup' style={{textDecoration:'none'}}><FlatButton label="Start a Broadcast" primary={true} style={styles.buttonSection}/></a>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
 var styles = {
-  image:{
-    'maxWidth': '300px'
-  }
+  titleFontBlack: {
+    fontWeight:'300',
+    fontFamily: 'Roboto, sans-serif',
+    color:'black',
+    textAlign: 'center'
+  },
+
+  flexColumn: {
+    display: 'flex',
+    flexDirection:'column',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    alignItem: 'center'
+  },
+
+  boxCenter: {
+    flex: 1,
+    alignSelf: 'center'
+  },
 }
 export default NoStream;
